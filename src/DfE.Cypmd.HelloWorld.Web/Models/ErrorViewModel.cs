97829909628d4306143a040aa397f778bd682726
Dave Gouge <davegouge@gmail.com>
2026-03-04 11:44:53 +0000

    Added placeholder Hello World app to aid in pipeline development

---
+++ src/DfE.Cypmd.HelloWorld.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DfE.Cypmd.HelloWorld.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

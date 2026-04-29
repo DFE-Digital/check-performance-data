0d9949b5ea0d8a252ae1bad9bfefc55cf35271c4
paul cripps <paul.cripps@gmail.com>
2026-04-29 12:21:38 +0100

    Removed old poc project files

--- src/DfE.CheckPerformanceData.ZendeskClient/Refit/Services/IZendeskAttachmentService.cs
+++
@@ -1,9 +0,0 @@
-﻿using DfE.CheckPerformanceData.Infrastructure.ZendeskClient.Models;
-
-namespace DfE.CheckPerformanceData.ZendeskClient.Refit.Services
-{
-    public interface IZendeskAttachmentService
-    {
-        Task<UpdateTicketResponse> AddAttachmentAsync(long ticketId, string fileName, Stream fileStream, string commentBody = "Attachment uploaded");
-    }
-}

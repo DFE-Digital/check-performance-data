e613f48b95d46df088cb35c10db441954b8d93d3
Dave Gouge <davegouge@gmail.com>
2026-05-21 15:32:28 +0100

    Hack to fix a migration name as something's gone horribly wrong in some environments. (#102)

--- src/DfE.CheckPerformanceData.Web/Extensions/MigrationExtensions.cs
+++ src/DfE.CheckPerformanceData.Web/Extensions/MigrationExtensions.cs
@@ -10,8 +10,21 @@ public static class MigrationExtensions
         // if (!app.Environment.IsDevelopment())
         //     return;
 
+
+        
         await using var scope = app.Services.CreateAsyncScope();
         var db = scope.ServiceProvider.GetRequiredService<PortalDbContext>();
+
+        // Some environments had AddChangeRequest applied under the old timestamp before the migration 
+        // file was renamed. Normalise the history so MigrateAsync sees it as already applied.         
+        await db.Database.ExecuteSqlRawAsync("""                                                       
+                                             UPDATE "__EFMigrationsHistory"                                                             
+                                             SET "MigrationId" = '20260520102855_AddChangeRequest'                                      
+                                             WHERE "MigrationId" = '20260518121634_AddChangeRequest'                                    
+                                             """);    
+        
         await db.Database.MigrateAsync();
+        
+        
     }
 }

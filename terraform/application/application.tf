44cb2e74e557147d083d86f86096a2b697158f68
paul cripps <paul.cripps@gmail.com>
2026-05-28 15:30:00 +0100

    fixed config name miss match

--- terraform/application/application.tf
+++ terraform/application/application.tf
@@ -23,8 +23,8 @@ module "application_configuration" {
     AZURE_STORAGE_ACCOUNT_NAME       = local.azure_storage_account_name
     AZURE_STORAGE_ACCESS_KEY         = local.azure_storage_access_key
     AZURE_STORAGE_CONTAINER          = local.azure_storage_container
-    ZendeskSettings__Email           = module.infrastructure_secrets.map.ZENDESK-API-EMAIL
-    ZendeskSettings__ApiToken        = module.infrastructure_secrets.map.ZENDESK-API-TOKEN
+    ZendeskSettings__Email           = module.infrastructure_secrets.map.ZendeskSettings__Email
+    ZendeskSettings__ApiToken        = module.infrastructure_secrets.map.ZendeskSettings__ApiToken
   }
 }
 

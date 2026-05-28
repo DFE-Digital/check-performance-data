b7b496d382e50dafff2b69add9c73f200fc04423
Dave Gouge <davegouge@gmail.com>
2026-05-28 14:53:02 +0100

    280047 Child Missing Education Journey. Also added optional radio button hint text

--- src/DfE.CheckPerformanceData.Application/Journey/QuestionOption.cs
+++ src/DfE.CheckPerformanceData.Application/Journey/QuestionOption.cs
@@ -4,5 +4,6 @@ public sealed class QuestionOption
 {
     public required string Value { get; init; }
     public required string Label { get; init; }
+    public string? SubLabel { get; init; }
     public string? NextPageId { get; init; }
 }

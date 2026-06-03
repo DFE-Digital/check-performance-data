f49c2ec1ca3fca5678456f193c9a261436482e11
Dave Gouge <davegouge@gmail.com>
2026-06-03 18:28:05 +0100

    Added storage connection options for admin

--- src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/StorageBlobPreviewViewModel.cs
+++ src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/StorageBlobPreviewViewModel.cs
@@ -2,6 +2,8 @@ namespace DfE.CheckPerformanceData.Web.Controllers.ViewModels;
 
 public sealed class StorageBlobPreviewViewModel
 {
+    public string AccountKey { get; init; } = string.Empty;
+    public string AccountDisplayName { get; init; } = string.Empty;
     public string ContainerName { get; init; } = string.Empty;
     public string BlobName { get; init; } = string.Empty;
     public string? ContentType { get; init; }
--- src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/StorageContainerListViewModel.cs
+++ src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/StorageContainerListViewModel.cs
@@ -2,5 +2,7 @@ namespace DfE.CheckPerformanceData.Web.Controllers.ViewModels;
 
 public sealed class StorageContainerListViewModel
 {
+    public string AccountKey { get; init; } = string.Empty;
+    public string AccountDisplayName { get; init; } = string.Empty;
     public IReadOnlyList<string> Containers { get; init; } = [];
 }

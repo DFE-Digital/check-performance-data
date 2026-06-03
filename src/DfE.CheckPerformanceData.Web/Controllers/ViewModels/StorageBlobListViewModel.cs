361adeb95f26a297abd758432df1c5f6867d201e
Dave Gouge <davegouge@gmail.com>
2026-06-03 11:48:24 +0100

    Added a quick storage browser for admin so we can check on what's uploaded by LDS.

---
+++ src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/StorageBlobListViewModel.cs
@@ -0,0 +1,15 @@
+namespace DfE.CheckPerformanceData.Web.Controllers.ViewModels;
+
+public sealed class StorageBlobListViewModel
+{
+    public string ContainerName { get; init; } = string.Empty;
+    public IReadOnlyList<StorageBlobItemViewModel> Blobs { get; init; } = [];
+}
+
+public sealed class StorageBlobItemViewModel
+{
+    public string Name { get; init; } = string.Empty;
+    public long SizeBytes { get; init; }
+    public string? ContentType { get; init; }
+    public DateTimeOffset? LastModified { get; init; }
+}

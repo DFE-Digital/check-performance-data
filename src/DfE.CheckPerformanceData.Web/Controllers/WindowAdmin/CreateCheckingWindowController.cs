e0d20f466d85c781120edaac7a6ce3a22037de6c
darkangel <gareth.gooch@education.gov.uk>
2026-07-06 00:48:33 +0100

    adding in the save to the DB and redirect

--- src/DfE.CheckPerformanceData.Web/Controllers/WindowAdmin/CreateCheckingWindowController.cs
+++ src/DfE.CheckPerformanceData.Web/Controllers/WindowAdmin/CreateCheckingWindowController.cs
@@ -12,6 +12,8 @@ public class CreateCheckingWindowController(ILogger<CreateCheckingWindowControll
     [HttpGet("admin/windows/create-checking-window")]
     public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
+        ViewBag.Cancelation = false;
+        
         CheckingWindowDraft? draft = HttpContext.Session.GetObject<CheckingWindowDraft>("CheckingWindowDraft");
 
         if (draft == null)
@@ -39,16 +41,15 @@ public class CreateCheckingWindowController(ILogger<CreateCheckingWindowControll
             return BadRequest("Invalid data");
         }
 
-        // CheckingWindowDto checkingWindowDto = new CheckingWindowDto()
-        // {
-        //     Title = draft.Title,
-        //     StartDate = draft.StartDate.Value,
-        //     EndDate = draft.EndDate.Value,
-        //     CheckingWindowType = draft.CheckingWindowType.Value
-        // };
-        
-        throw new NotImplementedException();
-        
-        return View(PageView, draft);
+        CheckingWindowDto checkingWindowDto = new CheckingWindowDto() 
+        {
+             Title = draft.Title,
+             StartDate = draft.StartDate.Value,
+             EndDate = draft.EndDate.Value,
+             CheckingWindowType = draft.CheckingWindowType.Value,
+             KeyStage = draft.KeyStage.Value,
+         };
+        CheckingWindowDto window = await windowService.CreateAsync(checkingWindowDto, cancellationToken);
+        return RedirectToAction("Index", "Summary", new { id = window.Id });
     }
 }

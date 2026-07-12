00907efd5dbc7a831010394debf31be5389aaca7
darkangel <gareth.gooch@education.gov.uk>
2026-07-12 12:52:00 +0100

    making pageurl settable as not always available at intialiasation

--- src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/WindowAdmin/AdminPage.cs
+++ src/DfE.CheckPerformanceData.Web/Controllers/ViewModels/WindowAdmin/AdminPage.cs
@@ -3,6 +3,6 @@ namespace DfE.CheckPerformanceData.Web.Controllers.ViewModels.WindowAdmin;
 public class AdminPage
 {
     public Guid WindowId { get; init; }
-    public string? PostUrl { get; init; } = string.Empty;
+    public string? PostUrl { get; set; } = string.Empty;
     public string? CancelUrl { get; init; } = string.Empty; 
 }

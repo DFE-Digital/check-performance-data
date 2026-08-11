c4b949c6a48e24805a312f18fd2a365cff5a9f0e
GG-DfE <GG-DfE@nomail.com>
2026-08-11 23:22:24 +0100

    adding in new view for ammendments across the est

--- src/DfE.CheckPerformanceData.Application/WindowManagement/IWindowService.cs
+++ src/DfE.CheckPerformanceData.Application/WindowManagement/IWindowService.cs
@@ -30,6 +30,15 @@ public sealed class CheckingWindowDto
     public string IngressFileChecksum { get; set; } = string.Empty;
     public string SchemaFile { get; set; } = string.Empty;
     public string SchemaFileChecksum { get; set; } = string.Empty;
+    public bool Validated { get; set; }
+    public DateTime? ValidatedAt { get; set; }
+    public bool IsOpen {
+        get
+        {
+            DateTimeOffset now = DateTime.UtcNow;
+            return (StartDate <= now.DateTime && now.DateTime <= EndDate);
+
+        } set; }
     public bool IsOpen { get; set; }
     public string TurnaroundCommitment { get; set; } = string.Empty;
 
